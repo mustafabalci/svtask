01a68d77b8cce8e8b9bf613cafbbe0a73df98ee7
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-20 23:06:49 +0300

    mock pages

---
+++ src/pages/PostDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostDetail = () => {
+  return (
+    <div>
+      <p>Post Detail</p>
+    </div>
+  );
+};
+
+export default PostDetail;
---
+++ src/pages/ProfileDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileDetail = () => {
+  return (
+    <div>
+      <p>Profile Detail</p>
+    </div>
+  );
+};
+
+export default ProfileDetail;
