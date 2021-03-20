9d0e0013b005fff8b333e3eba22a829e492b08da
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-20 23:07:35 +0300

    router configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import { Home, PostDetail, ProfileDetail } from "../pages";
+
+import "../styles/index.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Layout>
+        <Route path="/" component={Home} />
+        <Route path="/post-detail" component={PostDetail} />
+        <Route path="/profile-detail" component={ProfileDetail} />
+      </Layout>
+    </Router>
+  );
 };
 
 export default App;
