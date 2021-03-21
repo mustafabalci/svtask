d2ef8540bc83fd3ab78179fe19408e9386407564
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-21 13:32:17 +0300

    router configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import { Home, PostDetail, ProfileDetail } from "../pages";
 
@@ -9,9 +9,11 @@ const App = () => {
   return (
     <Router>
       <Layout>
-        <Route path="/" component={Home} />
-        <Route path="/post-detail" component={PostDetail} />
-        <Route path="/profile-detail" component={ProfileDetail} />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/post-detail" component={PostDetail} />
+          <Route path="/profile-detail" component={ProfileDetail} />
+        </Switch>
       </Layout>
     </Router>
   );
