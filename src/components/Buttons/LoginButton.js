5686cc6c5121316272315636bf95627a3d0ef085
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 01:23:47 +0300

    login button added

---
+++ src/components/Buttons/LoginButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Buttons.module.css";
+
+const LoginButton = () => {
+  return (
+    <div>
+      <button className={styles.LoginButton}>Login</button>
+    </div>
+  );
+};
+
+export default LoginButton;
