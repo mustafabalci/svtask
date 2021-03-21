7cc880017e329550b9bf483a5698858b9f3f7519
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 01:29:23 +0300

    home page component completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ListItem from "../components/ListItem/ListItem";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
-const Home = () => {
-  return (
-    <div>
-      <p>Home</p>
-    </div>
-  );
+const Home = ({ dispatch, loading, posts, failed }) => {
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const renderPosts = () => {
+    if (loading) return <p>İçerikler yükleniyor...</p>;
+    if (failed) return <p>Veri alınırken bir hata meydana geldi.</p>;
+    console.log(posts);
+    return posts.map((post) => (
+      <ListItem key={post.id} order={post.id} post={post} />
+    ));
+  };
+
+  return <>{renderPosts()}</>;
 };
 
-export default Home;
+const mapStateToProps = (state) => ({
+  loading: state.posts.loading,
+  posts: state.posts.posts,
+  failed: state.posts.failed,
+});
+
+export default connect(mapStateToProps)(Home);
