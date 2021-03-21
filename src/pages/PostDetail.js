52d066ab46767b41bf4a73b2c2be4892bfcb15c7
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 01:29:09 +0300

    post detail page completed

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-const PostDetail = () => {
+const PostDetail = ({ match, posts, loading }) => {
+  const [post, setPost] = useState();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!loading) {
+      setPost(posts.find((post) => post.id === Number(match.params.id)));
+    }
+  }, [posts]);
+
   return (
     <div>
-      <p>Post Detail</p>
+      {!post ? (
+        <p>Yükleniyor...</p>
+      ) : (
+        <>
+          <h1>{post.title}</h1>
+          <a onClick={() => history.push(`/profile-detail/${post.userId}`)}>
+            Yazar sayfasına git
+          </a>
+          <p>{post.body}</p>
+        </>
+      )}
     </div>
   );
 };
 
-export default PostDetail;
+const mapStateToProps = (state) => ({
+  loading: state.posts.loading,
+  posts: state.posts.posts,
+});
+export default connect(mapStateToProps)(PostDetail);
