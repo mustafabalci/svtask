56ff870d6c2e31c88610c98f7e6f8ccb3e722024
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 14:21:16 +0300

    fetch post early

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Modal.module.css";
 import { AiOutlineClose } from "react-icons/ai";
 import Button from "../Buttons/Button";
 import { connect } from "react-redux";
 import { closeModal } from "../../actions/modalActions";
+import { onUpdatePost } from "../../actions/postActions";
 
 const Modal = ({ dispatch, show, selected }) => {
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  useEffect(() => {
+    if (selected) {
+      setTitle(selected.title);
+      setBody(selected.body);
+    }
+  }, [selected]);
+
+  const updateModalPost = (e) => {
+    e.preventDefault();
+    dispatch(onUpdatePost({ id: selected.id, title, body }));
+  };
+
   return (
     <div
       className={styles.ModalWrapper}
@@ -22,36 +38,36 @@ const Modal = ({ dispatch, show, selected }) => {
           </span>
         </div>
         <div className={styles.ModalContent}>
-          {selected && (
-            <form className="mt-2">
-              <div className="d-flex flex-column">
-                <label htmlFor="title" className={styles.FormLabel}>
-                  Title
-                </label>
-                <input
-                  type="text"
-                  className={styles.FormControl}
-                  id="title"
-                  name="title"
-                  value={selected.title}
-                />
-              </div>
-              <div className="d-flex flex-column mt-2">
-                <label htmlFor="body" className={styles.FormLabel}>
-                  Body
-                </label>
-                <textarea
-                  className={styles.FormControl}
-                  id="body"
-                  rows="4"
-                  value={selected.body}
-                />
-              </div>
-            </form>
-          )}
+          <form className="mt-2">
+            <div className="d-flex flex-column">
+              <label htmlFor="title" className={styles.FormLabel}>
+                Title
+              </label>
+              <input
+                type="text"
+                className={styles.FormControl}
+                id="title"
+                name="title"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </div>
+            <div className="d-flex flex-column mt-2">
+              <label htmlFor="body" className={styles.FormLabel}>
+                Body
+              </label>
+              <textarea
+                className={styles.FormControl}
+                id="body"
+                rows="4"
+                onChange={(e) => setBody(e.target.value)}
+                value={body}
+              />
+            </div>
+          </form>
         </div>
         <div className={`${styles.ModalActions} d-inline-flex`}>
-          <Button text="GÜNCELLE" />
+          <Button text="GÜNCELLE" onClick={updateModalPost} />
         </div>
       </div>
     </div>
