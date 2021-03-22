bfa7222d01118d3c8d74f2d4c8bcc3d8ed42013c
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 14:20:30 +0300

    tooltip

--- src/components/SocialIcon/SocialIcon.js
+++ src/components/SocialIcon/SocialIcon.js
@@ -4,6 +4,7 @@ import styles from "./SocialIcon.module.css";
 const SocialIcon = ({ bgColor, icon }) => {
   return (
     <div className={styles.SocialIcon} style={{ backgroundColor: bgColor }}>
+      <div className={styles.Tooltip}>Follow us</div>
       {icon}
     </div>
   );
