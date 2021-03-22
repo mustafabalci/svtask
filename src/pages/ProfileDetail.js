705d7ee754f3f9f7bb05bb14961b57e016b1f9d5
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 11:57:44 +0300

    profile page

--- src/pages/ProfileDetail.js
+++ src/pages/ProfileDetail.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions/userActions";
+import Profile from "../components/Profile/Profile";
 
-const ProfileDetail = () => {
-  return (
-    <div>
-      <p>Profile Detail</p>
-    </div>
-  );
+const ProfileDetail = ({ dispatch, loading, user, failed }) => {
+  useEffect(() => {
+    console.log("asdadas", user);
+    dispatch(fetchUser(1));
+  }, [dispatch]);
+
+  const renderProfile = () => {
+    if (loading) {
+      return <p>Kullanıcı bilgisi yükleniyor...</p>;
+    }
+    if (failed) {
+      return <p>Kullanıcı bilgisi getirilirken bir problem oluştu.</p>;
+    }
+    if (user) {
+      return <Profile user={user} />;
+    }
+  };
+
+  return <>{renderProfile()}</>;
 };
 
-export default ProfileDetail;
+const mapStateToProps = (state) => ({
+  loading: state.user.loading,
+  user: state.user.user,
+  failed: state.user.failed,
+});
+
+export default connect(mapStateToProps)(ProfileDetail);
