a1a778672f88eaad66330e8be44d79aeccc88c94
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-22 14:20:23 +0300

    new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from "redux";
 
 import postsReducer from "./postReducer";
 import modalReducer from "./modalReducer";
+import userReducer from "./userReducer";
+import warnReducer from "./warnReducer";
 
 const rootReducer = combineReducers({
   posts: postsReducer,
   modal: modalReducer,
+  user: userReducer,
+  warnBox: warnReducer,
 });
 
 export default rootReducer;
